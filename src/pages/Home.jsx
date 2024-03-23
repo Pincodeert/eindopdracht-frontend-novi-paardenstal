bf065a9a5306079e2be279a3e9bea56f1aaadd54
Pincodeert <leonie.pin@gmail.com>
2024-03-23 12:43:38 +0100

    styling feature-section verder uitgewerkt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import almeria2 from '../assets/Almeria-2.JPG';
 import almeria3 from '../assets/Almeria-3.JPG';
 import almeria4 from '../assets/Almeria-4.JPG';
 import almeria5 from '../assets/Almeria-5.JPG';
-import nico from '../assets/Nico-wijs.jpg'
+import nico from '../assets/Nico-wijs.jpg';
+import star from '../assets/ster-image.jpg';
+import stable from '../assets/anna-kaminova-cCsbIhfjfTY-unsplash.jpg';
 
 function Home() {
 
@@ -61,6 +63,11 @@ function Home() {
                             consequuntur deserunt doloremque doloribus eligendi eos error esse laborum maxime natus non
                             omnis pariatur possimus, quis ratione soluta ullam ut voluptates voluptatibus.
                         </p>
+                            <div>
+                                <img className="star-image" src={star} alt="ster"/>
+                                <img className="star-image" src={star} alt="ster"/>
+                                <img className="star-image" src={star} alt="ster"/>
+                            </div>
                         </div>
                         <div className="intro-image-wrapper">
                             <span className="intro-image">
@@ -81,23 +88,23 @@ function Home() {
                         <h2>Waarom Blaze of Glory?</h2>
                         <div className="usp-article-wrapper">
                             <article className="usp-article">
-                                <img src={nico} alt="ster"/>
+                                <img src={star} alt="ster"/>
                                 <p>Comfortabele Binnen en Buiten Stallen</p>
                             </article>
                             <article className="usp-article">
-                                <img src={nico} alt="ster"/>
+                                <img src={star} alt="ster"/>
                                 <p>Keuze uit Half of Vol Pension</p>
                             </article>
                             <article className="usp-article">
-                                <img src={nico} alt="ster"/>
+                                <img src={star} alt="ster"/>
                                 <p>Glorieuze Buitenweide in een country-setting</p>
                             </article>
                             <article className="usp-article">
-                                <img src={nico} alt="ster"/>
+                                <img src={star} alt="ster"/>
                                 <p>Sterren-behandeling ook voor uw paarde(en)</p>
                             </article>
                             <article className="usp-article">
-                                <img src={nico} alt="ster"/>
+                                <img src={star} alt="ster"/>
                                 <p>Eenvoudige online aanmelding en afmelding</p>
                             </article>
                         </div>
@@ -110,11 +117,66 @@ function Home() {
                 {/*features-section*/}
                 <section className="outer-container feature-section">
                     <div className="inner-container">
-                        <h2>Welke voorzieningen?</h2>
-                        <div>
-                            <article></article>
-                            <article></article>
-                            <article></article>
+                        <div className="title-wrapper">
+                            <img className="star-image" src={star} alt="ster-icoon"/>
+                            <h2>Welke voorzieningen?</h2>
+                            <img className="star-image" src={star} alt="ster-iccon"/>
+                        </div>
+                        <div className="feature-article-wrapper">
+                            <article className="feature-article" >
+                                <div className="feature-text">
+                                    <h3>Onze stallen</h3>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test-plaatje"/>
+                                        <p>binnenstal klein (3x4) en groot (4x4)</p>
+                                    </div>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test-plaatje"/>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test"/>
+                                        <p>Lorem ipsum dolor sit amet</p>
+                                    </div>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test"/>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                </div>
+                                <div className="feature-image">
+                                    <span>
+                                    <img src={stable} alt="Image by Anna Kaminova-Unsplashed"/>
+                                    </span>
+                                    <button>naar de stallen</button>
+                                </div>
+                            </article>
+                            <article className="feature-article reversed-direction">
+                                <div className="feature-text">
+                                    <h3>Onze stallen</h3>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test-plaatje"/>
+                                        <p>binnenstal klein (3x4) en groot (4x4)</p>
+                                    </div>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test-plaatje"/>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test"/>
+                                        <p>Lorem ipsum dolor sit amet</p>
+                                    </div>
+                                    <div className="feature-text-item">
+                                        <img src={star} alt="test"/>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                </div>
+                                <div className="feature-image">
+                                    <span>
+                                    <img src={stable} alt="Image by Anna Kaminova-Unsplashed"/>
+                                    </span>
+                                    <button>naar de stallen</button>
+                                </div>
+                            </article>
                         </div>
                     </div>
                 </section>
@@ -155,7 +217,7 @@ function Home() {
                 </div>
             </footer>
         </>
-)
+    )
 }
 
 export default Home
