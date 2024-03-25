91b5a0e4c53e139a2bacd1e887fc65432b78faa6
Pincodeert <leonie.pin@gmail.com>
2024-03-25 17:17:44 +0100

    footer uitgewerkt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import './Home.css';
 import horse from '../assets/Paard-bewerkt-header.jpg'
 import almeria1 from '../assets/Almeria-1.JPG';
 import almeria2 from '../assets/Almeria-2.JPG';
@@ -220,14 +221,46 @@ function Home() {
                     </div>
                 </section>
             </main>
-            <footer className="outer-container footer-section">
-                <div className="inner-container">
-                    <section>
-                        <img src="" alt=""/>
-                        <div>
-                        <article></article>
-                            <article></article>
-                            <article></article>
+            <footer >
+                <div className="outer-container footer-section">
+                <section className="inner-container footer-info-section">
+                        <div className="title-wrapper">
+                            <img className="star-image" src={star} alt="ster-icoon"/>
+                            <img className="star-image" src={star} alt="ster-icoon"/>
+                            <img className="star-image" src={star} alt="ster-iccon"/>
+                        </div>
+                        <div className="footer-info-wrapper">
+                            <article className="footer-info">
+                                <div className="info-text-wrapper">
+                                <p className="footer-info-title">Openingstijden</p>
+                                <p>ma: 9.00u - 21.00u</p>
+                                <p>di: 8.00 - 22.00u</p>
+                                <p>wo: 8.00 - 22.00u</p>
+                                <p>do: 8.00 - 23.00u</p>
+                                <p>vr: 8.00 - 2.00u</p>
+                                <p>za: 9.00 - 2.00u</p>
+                                <p>zo: 9.00 - 23.00u</p>
+                                </div>
+                            </article>
+                            <article className="footer-info">
+                                <div className="info-text-wrapper">
+                                <p className="footer-info-title">Algemeen</p>
+                                <p>algemene voorwaarden</p>
+                                <p>privacy verklaring</p>
+                                <p>disclaimer</p>
+                                <p>klachtprocedure</p>
+                                </div>
+                            </article>
+                            <article className="footer-info">
+                                <div className="info-text-wrapper">
+                                <p className="footer-info-title">contact</p>
+                                <p>Landweg 2A</p>
+                                <p>1234 AB</p>
+                                <p className="footer-info-title">Parkstad</p>
+                                <p>tel 012 234 56 78</p>
+                                <p>info@blazeofglory.com</p>
+                                </div>
+                            </article>
                         </div>
                         <div>
                             <img src="" alt=""/>
@@ -235,11 +268,13 @@ function Home() {
                             <img src="" alt=""/>
                         </div>
                     </section>
-                    <section>
+                </div>
+                <div className="outer-container footer-section">
+                <section className="inner-container footer-close-section">
                         <p>
                             Copyright pensionstal Blaze of Glory | mede mogelijk gemaakt door Sportcentrum Nieuw-Vennep
                         </p>
-                    </section>
+                </section>
                 </div>
             </footer>
         </>
